8329dfc1b966c4e5b0c0a2deaf1806e48529e521
jasongreenRPI <greenberg.jason@outlook.com>
2025-04-27 16:07:07 -0400

    .

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -2,16 +2,18 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  publicPath: process.env.DEPLOY_ENV === 'GH_PAGES'
-    ? '/admin-one-vue-tailwind/'
-    : '/',
+  publicPath:
+    process.env.DEPLOY_ENV === "GH_PAGES" ? "/admin-one-vue-tailwind/" : "/",
 
   // Remove moment.js from chart.js
-  configureWebpack: config => {
+  configureWebpack: (config) => {
     return {
       externals: {
-        moment: 'moment'
-      }
-    }
-  }
-}
+        moment: "moment",
+      },
+    };
+  },
+
+  // Disable linting
+  lintOnSave: false,
+};
